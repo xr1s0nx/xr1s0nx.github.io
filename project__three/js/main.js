9ef52cb71e1c03e4a3a333092b14150170cb0535
xr1s0nx <58141852+xr1s0nx@users.noreply.github.com>
2022-03-21 23:00:16 +0300

    Add files via upload

---
+++ project__three/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 1,
+        arrows: false,
+        infinite: true,
+        autoPlay: true,
+        adaptiveHeight: true
+    });
+});
